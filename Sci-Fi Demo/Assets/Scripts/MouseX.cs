6ab0086adcf38409ec5e9b85875c8556e44c15be
JeanCho <nicetoto97@naver.com>
2019-11-26 18:28:06 +0900

    Added Player control, lighting

---
+++ Sci-Fi Demo/Assets/Scripts/MouseX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseX : MonoBehaviour
+{
+    
+    public float Xsensitivity = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += mouseX * Xsensitivity;
+        transform.localEulerAngles = newRotation;
+        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,
+        //    transform.localEulerAngles.y+( mouseX*Xsensitivity),
+        //    transform.localEulerAngles.z );
+    }
+}
